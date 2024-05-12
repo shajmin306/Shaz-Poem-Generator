600144709a1c1c254ea8b19956012992daa5cdf8
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-05-12 15:06:37 +0100

    corrected JS error

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "c9b30483aedf4foe2dd664a0ftc74778";
   let context =
-    "You are a polite and romantic poem expert and love to write short poems. Please generate a four line poem, in basic HTML and separate each line using <br/>. Please follow user instructions";
+    "You are a romantic poem expert and love to write short poems. Please generate a four line poem, and separate each line using <br/>. Please follow user instructions";
   let prompt = `User instructions: Please generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
