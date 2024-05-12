080dda1c938bee5fe2f0ceddd672f956a608533f
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-05-12 16:07:48 +0100

    Improved JS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 10,
+    delay: 50,
     cursor: "",
   });
 }
@@ -16,6 +16,10 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Please generate a four line poem, and separate each line using <br/>. Please follow user instructions";
   let prompt = `User instructions: Please generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Coming up with a poem about ${instructionsInput.value}...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
